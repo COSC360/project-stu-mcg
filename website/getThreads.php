8debec853aec6fe1b0cea579b6fb58c085d46f52
stu <stumcg@student.ubc.ca>
2023-03-24 14:53:40 -0700

    removed stuff I dont need from getThreads

--- website/getThreads.php
+++ website/getThreads.php
@@ -35,39 +35,4 @@
         echo(json_encode($jsonResult));
     }
     $conn->close();
-    die();
 ?>
-
-<?php
-            if ($stmt->execute()) {
-                $result = $stmt->get_result();
-                $count = 0;
-                $more = false;
-                while($thread = $result->fetch_assoc()){
-                    $count++;
-                    if($count == 11){
-                        $more = true;
-                        break;
-                    }
-                    $id = $thread['threadId'];
-                    $title = $thread['threadTitle'];
-                    $author = $thread['threadAuthor'];
-                    $text = $thread['threadText'];
-                    echo "<div class='thread'>"; // Start of block
-                    echo("<a href='thread.php?id=" . $thread['threadId'] . "'>");
-                    echo "<h2 >{$title}</h2></a>"; // Thread title in bold
-                    if(isset($_SESSION['isAdmin'])){
-                        echo "<form action='deleteThread.php' method='POST'>";
-                        echo "<input type='hidden' name='threadId' value='".$id."'>";
-                        echo "<input class='delete'type='submit' value='Delete'>";
-                        echo "</form>";
-                    }   
-                    echo "<p class='author' >Author: {$author}</p>"; // Author underneath
-                    echo "<p>{$text}</p>"; // Preview of thread text
-                    echo "</div>"; 
-                }
-            }else {
-                echo "Error: " . $stmt->error;
-            }
-            $conn->close();
-        ?>
