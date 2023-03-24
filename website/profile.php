ef54daade1e53ee9441016eb9664645c2ca67f9a
stu <stumcg@student.ubc.ca>
2023-03-23 22:28:20 -0700

    added correct recent threads and post count to profile as well as support for looking at other users profiles

--- website/profile.php
+++ website/profile.php
@@ -11,27 +11,62 @@
                 <a id = "editLink" href="#">edit profile</a>
 
                 <?php 
-                    if(isset($_SESSION['username'])){
-                        echo '<h1>'. $_SESSION['username'].'</h1>';
-                        if($result = glob("./userImages/" . $_SESSION['username'] . ".*")){
-                            echo("<img class='profilePic' src='{$result[0]}'>");
-                        } else {
-                            echo("<img class='profilePic' src='img/blank-profile-picture-973460_1280.png'>");
-                        }
+                    if(isset($_GET['user'])){
+                        $username = $_GET['user'];
+                    }else if(isset($_SESSION['username'])){
+                        $username = $_SESSION['username'];
+                    }else{
+                        die("null user");
                     }
-                    
+                    include('dbConnection.php');
+                    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+                    $stmt->bind_param("s", $username);
+                    if($stmt->execute()){
+                        $result = $stmt->get_result();
+                        if(!$result->fetch_assoc()){
+                            die("user does not exist");
+                        }
+                    } else {
+                        die("Error: " . $stmt->error);
+                    }
+                    echo '<h1>'. $username.'</h1>';
+                    if($result = glob("./userImages/" . $username . ".*")){
+                        echo("<img class='profilePic' src='{$result[0]}'>");
+                    } else {
+                        echo("<img class='profilePic' src='img/blank-profile-picture-973460_1280.png'>");
+                    }
                 ?>
                 <h2>Bio:</h2>
                 <p>Super cool person!</p>
                 <h2>Location: Kelowna BC</h2>
-                <h2>Post Count: 3333</h2>
+                <h2>
+                    Post Count:
+                    <?php
+                        $stmt = $conn->prepare('SELECT (SElECT COUNT(threadId) FROM threads WHERE threadAuthor = ?) AS threadCount, (SELECT COUNT(replyId) FROM replies WHERE replyAuthor = ?) AS replyCount');
+                        $stmt->bind_param('ss' , $username, $username);
+                        if($stmt->execute()){
+                            $result = $stmt->get_result();
+                            $value = $result->fetch_assoc();
+                            echo($value['threadCount'] + $value['replyCount']);
+                        }else{
+                            die($stmt->error);
+                        }
+                    ?>
+                </h2>
                 <h2>Recent threads:</h2>
                 <ul>
-                    <li><a href="#">Thread 1</a></li>
-                    <li><a href="#">Thread 2</a></li>
-                    <li><a href="#">Thread 3</a></li>
-                    <li><a href="#">Thread 4</a></li>
-                    <li><a href="#">Thread 5</a></li>
+                    <?php
+                        $stmt = $conn->prepare('SELECT * FROM threads WHERE threadAuthor = ? ORDER BY threadDate DESC LIMIT 5');
+                        $stmt->bind_param('s', $username);
+                        if($stmt->execute()){
+                            $result = $stmt->get_result();
+                            while($thread = $result->fetch_assoc()){
+                                echo("<a href = 'thread.php?id=" . $thread['threadId'] . "><li>" . $thread['threadTitle'] . "</li></a>");
+                            }
+                        }else{
+                            die($stmt->error);
+                        }
+                    ?>
                 </ul>
                 <?php
                     if(isset($_POST['logout'])){
@@ -51,5 +86,6 @@
                 </form>
             </div>
         </main>
+        <?php $conn->close();?>
     </body>
 </html>
