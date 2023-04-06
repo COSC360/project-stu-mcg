df90a8fcbee3cd3daeeb8b8da29c39f255ef8f75
mvibert1 <macvibert@gmail.com>
2023-04-06 14:57:30 -0700

    added table to display top 10 most engaged threads to stats page

--- website/stats.php
+++ website/stats.php
@@ -13,6 +13,23 @@ while ($row = $result->fetch_assoc() ) {
   $data[] = array($row['region'], (int)$row['num_threads']);
 }
 $json_data = json_encode($data);
+
+$stmt2 = $conn->prepare("SELECT threads.threadId, threads.threadTitle, COUNT(replies.replyId) AS replyCount
+FROM threads
+LEFT JOIN replies ON threads.threadId = replies.thread
+GROUP BY threads.threadId
+ORDER BY replyCount DESC
+LIMIT 10
+");
+if ($stmt2->execute()) {
+  $result = $stmt2->get_result();
+}
+$data2 = array();
+$data2[] = array('Thread title', 'Number of replies');
+while ($row = $result->fetch_assoc() ) {
+  $data2[] = array($row['threadTitle'], (int)$row['replyCount']);
+}
+$json_data2 = json_encode($data2);
 $conn->close();
 ?>
 <html>
@@ -20,7 +37,7 @@ $conn->close();
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
     // google chart stuff
-      google.charts.load('current', {'packages':['corechart']});
+      google.charts.load('current', {'packages':['corechart', 'table']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
@@ -33,13 +50,37 @@ $conn->close();
 
         var chart = new google.visualization.PieChart(document.getElementById('chart'));
         chart.draw(data, options);
+
+        // Call drawTable() function here
+        drawTable();
+      }
+
+      function drawTable() {
+        var data = google.visualization.arrayToDataTable(<?php echo $json_data2; ?>);
+
+        var options = {
+          showRowNumber: true,
+          width: '60%',
+          height: '100%'
+        };
+
+        var table = new google.visualization.Table(document.getElementById('chart2'));
+
+        table.draw(data, options);
       }
     </script>
     <link rel="stylesheet" href="css/all.css">
     <link rel="stylesheet" href="css/stats.css">
   </head>
   <?php include('header.php'); ?>
+  <?php
+            if(!isset($_SESSION['isAdmin'])){
+                die("Must be signed in as admin user to view this page");
+            }
+  ?>
   <body>
-    <div id="chart" ></div>
+    <div id="chart"></div>
+    <h2>Top 10 Threads by engagement: </h2>
+    <div id="chart2"></div>
   </body>
 </html>
