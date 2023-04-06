3b3ef8db5b3757cde0087e18fbf882b4d86c24cb
mvibert1 <macvibert@gmail.com>
2023-04-06 00:50:19 -0700

    more refactoring

--- website/tests/testDbConnection.php
+++ website/tests/testDbConnection.php
@@ -1,7 +1,11 @@
 <?php 
 class testDbConnection extends PHPUnit\Framework\TestCase{
-    public function testDbConnection(){ 
-        include('../dbConnection.php');
+    public function testDbConnection(){ //mock function from dbConnection file
+        $servername = "localhost";
+        $username_db = "cosc360user";
+        $password_db = "1234";
+        $dbname = "cosc360project";
+        $conn = new mysqli($servername, $username_db, $password_db, $dbname);
 
         $this->assertInstanceOf(mysqli::class, $conn);
         $this->assertTrue($conn->ping()); //ping to see if it's still connected
